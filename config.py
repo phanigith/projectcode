75abe1e381a635ea9f075248f42cf8b57f5b6811
phanigith <113577490+phanigith@users.noreply.github.com>
2022-12-03 20:42:37 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "mysqldb.cvie7on169tp.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "phaniproject"
+custombucket = "employeeinfos3"
+customregion = "us-east-1"
